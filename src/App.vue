473083b70b9d49d9e8c59a7d7b9f2d8c73b0a8fa
worapon <woraponok@outlook.com>
2017-03-15 00:06:13 +0700

    วางโครง

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,41 @@
 <template>
   <div id="app">
-    <section class="hero is-primary is-medium">
-      <!-- Hero header: will stick at the top -->
-      <div class="hero-head">
-        <header class="nav">
-          <div class="container">
-            <div class="nav-left">
-              <a class="nav-item">
-                <img src="images/bulma-type-white.png" alt="Logo">
-              </a>
-            </div>
-            <span class="nav-toggle">
-              <span></span>
-              <span></span>
-              <span></span>
-            </span>
-            <div class="nav-right nav-menu">
-              <a class="nav-item is-active">
-                Home
-              </a>
-              <a class="nav-item">
-                Examples
-              </a>
-              <a class="nav-item">
-                Documentation
-              </a>
-              <span class="nav-item">
-                <a class="button is-primary is-inverted">
-                  <span class="icon">
-                    <i class="fa fa-github"></i>
-                  </span>
-                  <span>Download</span>
-                </a>
-              </span>
-            </div>
-          </div>
-        </header>
+    <nav class="nav has-shadow">
+      <div class="container">
+        <div class="nav-left">
+          <a class="nav-item">
+            <img src="http://bulma.io/images/bulma-logo.png" alt="Bulma logo">
+          </a>
+          <a class="nav-item is-hidden-mobile is-tab is-active">Home</a>
+          <a class="nav-item is-hidden-mobile">Features</a>
+          <a class="nav-item is-hidden-mobile">Pricing</a>
+          <a class="nav-item is-hidden-mobile">About</a>
+        </div>
+        <span class="nav-toggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+        <div class="nav-right nav-menu">
+          <a class="nav-item is-hidden-tablet is-active">Home</a>
+          <a class="nav-item is-hidden-tablet">Features</a>
+          <a class="nav-item is-hidden-tablet">Pricing</a>
+          <a class="nav-item is-hidden-tablet">About</a>
+          <a class="nav-item">
+            <figure class="image is-16x16" style="margin-right: 8px;">
+              <img src="http://bulma.io/images/jgthms.png">
+            </figure>
+            Profile
+          </a>
+          <a class="nav-item">Log out</a>
+        </div>
       </div>
+    </nav>
 
+    <section class="hero is-primary">
       <!-- Hero content: will be in the middle -->
       <div class="hero-body">
-        <div class="container has-text-centered">
+        <div class="container">
           <h1 class="title">
             Title
           </h1>
@@ -66,9 +61,42 @@
         </nav>
       </div>
     </section>
-
-    
-
+    <nav class="nav has-shadow">
+      <div class="container">
+        <div class="nav-left">
+          <a class="nav-item is-tab " href="http://bulma.io/documentation/components/card/">
+            Card
+          </a>
+          <a class="nav-item is-tab " href="http://bulma.io/documentation/components/level/">
+            Level
+          </a>
+          <a class="nav-item is-tab " href="http://bulma.io/documentation/components/media-object/">
+            Media object
+          </a>
+          <a class="nav-item is-tab " href="http://bulma.io/documentation/components/menu/">
+            Menu
+          </a>
+          <a class="nav-item is-tab " href="http://bulma.io/documentation/components/message/">
+            Message
+          </a>
+          <a class="nav-item is-tab " href="http://bulma.io/documentation/components/modal/">
+            Modal
+          </a>
+          <a class="nav-item is-tab " href="http://bulma.io/documentation/components/nav/">
+            Nav
+          </a>
+          <a class="nav-item is-tab " href="http://bulma.io/documentation/components/pagination/">
+            Pagination
+          </a>
+          <a class="nav-item is-tab " href="http://bulma.io/documentation/components/panel/">
+            Panel
+          </a>
+          <a class="nav-item is-tab is-active" href="http://bulma.io/documentation/components/tabs/">
+            Tabs
+          </a>
+        </div>
+      </div>
+    </nav>
 
 
     <router-link to="/" tag="a" active-class="active" exact>รถเข็นของฉัน</router-link>
