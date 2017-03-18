c7bca8eee6e35b0f5e42578f00d1e3054c697cba
worapon <woraponok@outlook.com>
2017-03-18 11:29:13 +0700

    reface scrim

--- src/components/customer/mainTabs/Shrimp.vue
+++ src/components/customer/mainTabs/Shrimp.vue
@@ -1,16 +1,12 @@
 <template>
   <div>
-    <nav class="nav has-shadow">
-      <div class="container">
-        <div class="nav-left">
-          <router-link :to="{ path: '/shrimp' }" tag="a" class="nav-item is-tab" active-class="is-active" exact>ทั้งหมด</router-link>
-          <router-link :to="{ path: '/shrimp/กุ้งแคระ' }" tag="a" class="nav-item is-tab" active-class="is-active">กุ้งแคระ</router-link>
-          <router-link :to="{ path: '/shrimp/กุ้งเครฟิช' }" tag="a" class="nav-item is-tab" active-class="is-active">กุ้งเครฟิช</router-link>
-          <router-link :to="{ path: '/shrimp/กุ้งเรดบี' }" tag="a" class="nav-item is-tab" active-class="is-active">กุ้งเรดบี</router-link>
-          <router-link :to="{ path: '/shrimp/กุ้งชนิดอื่นๆ' }" tag="a" class="nav-item is-tab" active-class="is-active">กุ้งชนิดอื่นๆ</router-link>
-        </div>
-      </div>
-    </nav>
+    <NavBar>
+        <router-link :to="{ path: '/shrimp' }" tag="a" class="nav-item is-tab" active-class="is-active" exact>ทั้งหมด</router-link>
+        <router-link :to="{ path: '/shrimp/กุ้งแคระ' }" tag="a" class="nav-item is-tab" active-class="is-active">กุ้งแคระ</router-link>
+        <router-link :to="{ path: '/shrimp/กุ้งเครฟิช' }" tag="a" class="nav-item is-tab" active-class="is-active">กุ้งเครฟิช</router-link>
+        <router-link :to="{ path: '/shrimp/กุ้งเรดบี' }" tag="a" class="nav-item is-tab" active-class="is-active">กุ้งเรดบี</router-link>
+        <router-link :to="{ path: '/shrimp/กุ้งชนิดอื่นๆ' }" tag="a" class="nav-item is-tab" active-class="is-active">กุ้งชนิดอื่นๆ</router-link>
+    </NavBar>
     
     <div class="container">
       {{ id }}
@@ -19,7 +15,12 @@
 </template>
 
 <script>
+  import NavBar from '../NavBar.vue'
+
   export default {
+    components: {
+      NavBar,
+    }, 
     data () {
       return {
         id: null,
