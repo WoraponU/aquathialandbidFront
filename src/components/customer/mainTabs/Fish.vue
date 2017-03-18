e3af8e291a025a8704a4465d1787e08dcb3126a9
worapon <woraponok@outlook.com>
2017-03-19 00:22:50 +0700

    สร้างการ์ด

--- src/components/customer/mainTabs/Fish.vue
+++ src/components/customer/mainTabs/Fish.vue
@@ -11,91 +11,28 @@
       <router-link :to="{ path: '/fish/ปลาชนิดอื่นๆ' }" tag="a" class="nav-item is-tab" active-class="is-active" exact>ปลาชนิดอื่นๆ</router-link>
     </SubNavBar>
     
-    <section class="section">
-      <div class="container">
-        <div class="columns">
-          <div class="column is-one-third">
-            <div class="card">
-              <div class="card-image">
-                <figure class="image is-4by3">
-                  <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Image">
-                </figure>
-              </div>
-              <div class="card-content">
-                <div class="media">
-                  <div class="media-left">
-                    <figure class="image is-48x48">
-                      <img src="http://bulma.io/images/placeholders/96x96.png" alt="Image">
-                    </figure>
-                  </div>
-                  <div class="media-content">
-                    <p class="title is-4">John Smith</p>
-                    <p class="subtitle is-6">@johnsmith</p>
-                  </div>
-                </div>
-
-                <div class="content">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                  <a>#css</a> <a>#responsive</a>
-                  <br>
-                  <small>11:09 PM - 1 Jan 2016</small>
-                </div>
-              </div>
-            </div>
-          </div>  
-        </div>
-
-        <div class="columns">
-          <div class="column is-one-third">
-            <div class="card">
-              <div class="card-image">
-                <figure class="image is-4by3">
-                  <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Image">
-                </figure>
-              </div>
-              <div class="card-content">
-                <div class="media">
-                  <div class="media-left">
-                    <figure class="image is-48x48">
-                      <img src="http://bulma.io/images/placeholders/96x96.png" alt="Image">
-                    </figure>
-                  </div>
-                  <div class="media-content">
-                    <p class="title is-4">John Smith</p>
-                    <p class="subtitle is-6">@johnsmith</p>
-                  </div>
-                </div>
-
-                <div class="content">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                  <a>#css</a> <a>#responsive</a>
-                  <br>
-                  <small>11:09 PM - 1 Jan 2016</small>
-                </div>
-              </div>
-            </div>
-          </div>  
-        </div>
-
-        
-        {{ id }}
-      </div>
-    </section>
+    <PageBody>
+      <Card :cardData="testData"></Card>
+      {{ id }}
+    </PageBody>
   </div>
 </template>
 
 <script>
+  import PageBody from '../PageBody.vue'
   import SubNavBar from '../SubNavBar.vue'
+  import Card from '../Card.vue'
 
   export default {
     components: {
       SubNavBar,
+      PageBody,
+      Card,
     },
     data () {
       return {
         id: null,
+        testData: [{a:'a'},{b:'b'}],
       }
     },
     created() {
