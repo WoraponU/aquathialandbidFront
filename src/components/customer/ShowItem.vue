e48705870d8f67bf85d1e16c614eaf4dda5d81c0
worapon <woraponok@outlook.com>
2017-03-21 23:42:33 +0700

    จัด layout show item

--- src/components/customer/ShowItem.vue
+++ src/components/customer/ShowItem.vue
@@ -1,3 +1,20 @@
 <template>
-  <div>show item</div>
+  <div class="tile is-ancestor box">
+    <article class="tile is-parent is-vertical">
+      <p class="title">Wide column</p>
+      <p class="subtitle">Aligned with the right column</p>
+      <div class="tile is-child">
+        <div class="tile">
+          <div class="tile">image</div>
+          <div class="tile">info</div>
+        </div>
+      </div>
+      <div class="tile is-child">
+      ตารางประมูล
+      </div>
+      <div class="tile is-child">
+      รายละเอียดสินค้า
+      </div>
+    </article>
+  </div>
 </template>
